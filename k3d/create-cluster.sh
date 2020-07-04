4356a4eb756fe3eea064292bb1889d89ee9924c1
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-07-05 01:28:50 +0200

    feat. k3d-install :  Ajout de la recette d'installation K3S avec 3 masters  Ajout de la recette d'installation kubectl Ajout de la recette de création du cluster avec [k3d create]

--- k3d/create-cluster.sh
+++ k3d/create-cluster.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# You can redefine from outsite using 'export K3D_VERSION=theversionyouwant'
-k3d create cluster topgunCluster --masters 5 --workers 9
+# This stil deoes not work properly... July 2020
+k3d create cluster topgunCluster --masters 5 --workers 9 --api-port "0.0.0.0:6551"
